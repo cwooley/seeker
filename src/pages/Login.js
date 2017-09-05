bb8f141637c915f7375da50e126d23f8851818a5
Charles Wooley <cwooley89@gmail.com>
2017-09-05 13:34:13 -0400

    Added validations and error indications to sign up form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import { Grid, Input, Menu, Segment, Button, Dropdown } from 'semantic-ui-react'
+import { Grid, Input, Menu, Segment, Button, Dropdown, Message } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { fetchJWT, fetchUserData, createNewUser } from '../actions/users.js'
@@ -11,7 +11,8 @@ class Login extends Component {
     username: '',
     password: '',
     email: '',
-    profileImage: ''
+    profileImage: '',
+    errors: false
   }
 
   componentDidMount(){
@@ -23,20 +24,53 @@ class Login extends Component {
   }
 
   signUpBtnClicked = () => {
-    this.props.createNewUser(this.state)
+    if (!this.usernameError() && !this.passwordError() && !this.emailError()){
+      this.props.createNewUser(this.state)
+    } else {
+      this.setState({errors: true})
+    }
   }
 
+  displayError(){
+    let errList = []
+    if (this.usernameError()){
+      errList.push("Username must be 4 or more characters")
+    }
+    if (this.passwordError()){
+      errList.push("Password must be 4 or more characters")
+    }
+    if (this.emailError()){
+      errList.push("must be valid email")
+    }
+    if (this.state.errors && errList.length > 0 ) return  <Message error header='There was some errors with your submission' list={errList} />
+
+  }
   usernameChanged = (event) => {
     this.setState({ username: event.target.value })
+  }
+
+  usernameError = () => {
+    return this.state.username.length > 3 ? false : true
   }
 
   passwordChanged = (event) => {
     this.setState({ password: event.target.value })
   }
 
+  passwordError = () => {
+    return this.state.password.length > 3 ? false : true
+  }
+
   emailChanged = (event) => {
     this.setState({ email: event.target.value })
   }
+
+  emailError = () => {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return !re.test(this.state.email);
+  }
+
+
 
   profileImageChanged = (event) => {
     this.setState({ profileImage: event.target.value })
@@ -56,11 +90,11 @@ class Login extends Component {
     } else if (this.state.activeItem === 'SignUp'){
       return (
         <div>
-          <Input onChange={this.usernameChanged} fluid label='Username' value={this.state.username} placeholder='username' />
+          <Input onChange={this.usernameChanged} fluid label='Username' error={this.usernameError()} value={this.state.username} placeholder='username' />
           <br />
-          <Input onChange={this.passwordChanged} fluid label='Password' type='password' value={this.state.password} placeholder='password' />
+          <Input onChange={this.passwordChanged} fluid label='Password' error={this.passwordError()} type='password' value={this.state.password} placeholder='password' />
           <br />
-          <Input onChange={this.emailChanged} fluid label='Email' value={this.state.email} placeholder='email' />
+          <Input onChange={this.emailChanged} fluid label='Email' error={this.emailError()} value={this.state.email} placeholder='email' />
           <br />
           <Input onChange={this.profileImageChanged} fluid label='Profile Image Url' value={this.state.profileImage} placeholder='profileImage' />
           <br />
@@ -79,6 +113,7 @@ class Login extends Component {
         <Grid.Column width='2' floated='left' color='blue' >
         </Grid.Column>
         <Grid.Column width='4'>
+          {this.displayError()}
           <Menu attached='top' tabular className="loginForm">
             <Menu.Item name='Login' active={activeItem === 'Login'} onClick={this.handleItemClick} />
             <Menu.Item name='SignUp' active={activeItem === 'SignUp'} onClick={this.handleItemClick} />
